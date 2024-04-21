4b8c139db2af05ea98bd7324aeb246b852af8a21
Paola-Santanna <paolinhasj@gmail.com>
2024-04-21 11:02:15 -0300

    Add project files.

---
+++ Acréscimo no Salário/Program.cs
@@ -0,0 +1,6 @@
+﻿//Faça um programa que leia o valor atual do salário de uma pessoa e o valor com acréscimo. E informe a porcentagem de acréscimo que foi dada; atenção: salário somente aumenta; 
+
+//Início
+//Declaração de Variáveis
+//Entrada de Dados e Operações
+//Final do Programa
